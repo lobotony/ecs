0a69f1d1b56b91dad6d1890d387343992c6e429d
Tony Kostanjsek <lobotony666@yahoo.de>
2016-01-16 17:08:48 +0100

    cleanup, added ecs stub

---
+++ entitycomponentsystem.c
@@ -0,0 +1,1 @@
+#include "entitycomponentsystem.h"
--- main.c
+++ main.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
-
-#include "array.h"
-#include "log.h"
-#include "objectidpool.h"
 #include "test.h"
-#include "componentpool.h"
-
 
 int main(int argc, const char * argv[])
 {
