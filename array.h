50a6a5ad7e31362c229deca4a7b7d69be0da2089
Tony Kostanjsek <lobotony666@yahoo.de>
2016-01-16 13:24:29 +0100

    added ArrayElementPointer

--- array.h
+++ array.h
@@ -14,4 +14,5 @@ Array* ArrayInit(Array* array, size_t elementSize, size_t num);
 void ArrayDeinit(Array* array);
 void ArrayRealloc(Array* array, size_t newNum);
 
+#define ArrayElementPointer(arrayp, idx) ((arrayp)->data + idx*(arrayp)->elementSize)
 #define ArrayElement(arrayp, type, idx) ((type*)((arrayp)->data))[idx]
